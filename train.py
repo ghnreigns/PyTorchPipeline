b4c4d82490acb3360e92fc159167833bcdbebe86
Hongnan <hongnang.sph@gmail.com>
2021-03-10 16:46:31 +0800

    1
    1

--- train.py
+++ train.py
@@ -381,18 +381,7 @@ def train_on_fold(df_folds: pd.DataFrame, config, fold: int):
 
     train_df = df_folds[df_folds["fold"] != fold].reset_index(drop=True)
     val_df = df_folds[df_folds["fold"] == fold].reset_index(drop=True)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> parent of 51c2bf4 (1)
-=======
->>>>>>> parent of 51c2bf4 (1)
-    val_df.to_csv("val_df.csv")
-    # print(len(val_df))
->>>>>>> parent of 51c2bf4 (1)
     data_dict = {
         "dataset_train_dict": {
             "df": train_df,
